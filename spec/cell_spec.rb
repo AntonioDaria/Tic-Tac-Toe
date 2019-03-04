6847bb212e4526c7fb51d61f7570bc60b73cdc9d
antonio <antoniodaria@msn.com>
2019-03-04 22:31:12 +0000

    refactor cell test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 module TicTacToe
   describe Cell do
+    subject(:cell) { described_class.new }
 
     context "#initialize" do
       it "is initialized with a value of '' by default" do
-        cell = Cell.new
         expect(cell.value).to eq ''
       end
     end
